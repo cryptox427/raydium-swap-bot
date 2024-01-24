e3c7149b9c790fca1ff469bd57c9332c5edecc25
Samurai <127001sudais01@gmail.com>
2024-01-24 15:54:43 +0800

    chnged parsing format for keypair for myself for your use just uncomment line 19.

--- src/config.js
+++ src/config.js
@@ -1,19 +1,27 @@
 const { Connection, Keypair } = require("@solana/web3.js");
 const base58 = require("bs58");
-
 require("dotenv").config();
 
-const rpcUrl = process.env.RPC_URL;
-const wssUrl = process.env.RPC_URL.replace("https", "wss");
-const exitTarget = process.env.EXIT_TARGET;
-const buyAmount = process.env.BUY_AMOUNT;
-const connection = new Connection(rpcUrl);
+// constants
 const RAYDIUM_PROTOCOL = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
 const RAYDIUM_API = "https://api.dexscreener.com/latest/dex/tokens/";
 const WSOL = "So11111111111111111111111111111111111111112";
-const wallet = Keypair.fromSecretKey(base58.decode(process.env.WALLET_KEY));
 const TEST_LOOP = 5;
 
+// .env
+const rpcUrl = process.env.RPC_URL;
+const wssUrl = rpcUrl.replace("https", "wss");
+const exitTarget = process.env.EXIT_TARGET;
+const buyAmount = process.env.BUY_AMOUNT;
+
+// Solana connection and wallet
+const connection = new Connection(rpcUrl);
+// const wallet = Keypair.fromSecretKey(base58.decode(process.env.WALLET_KEY));
+const wallet = Keypair.fromSecretKey(
+  new Uint8Array(JSON.parse(process.env.WALLET_KEY))
+);
+
+// modules
 module.exports = {
   connection,
   RAYDIUM_PROTOCOL,
@@ -23,5 +31,5 @@ module.exports = {
   wssUrl,
   exitTarget,
   buyAmount,
-  TEST_LOOP
+  TEST_LOOP,
 };
